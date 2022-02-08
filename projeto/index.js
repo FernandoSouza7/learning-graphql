5fa1a2cd1ce2d99a3bfe25d4f45f896c6ce9eda9
matheus-fernando <matheus.fernando.coder@gmail.com>
2022-02-08 17:39:41 -0300

    My first query

--- projeto/index.js
+++ projeto/index.js
@@ -1,1 +1,25 @@
-console.log('Hello, GraphQl');
+const { ApolloServer, gql } = require('apollo-server'); 
+
+const typeDefs = gql`
+    # Pontos de entrada da sua API!
+    type Query { 
+        ola: String
+    }
+` 
+
+const resolvers = {
+    Query: {
+        ola() {
+            return 'Bom diaa';
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+})
+
+server.listen().then(({url}) => {
+    console.log(`Executando em ${url}`); 
+})
